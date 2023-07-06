8b0a36d214cc56256c1bce876b9f718ebef84f25
Emmaibe <emmanuelibenwankwo@gmail.com>
2023-07-06 20:52:01 +0100

    production

--- src/States/Bag.jsx
+++ src/States/Bag.jsx
@@ -2,7 +2,7 @@ function Bag(props) {
   const container = {
     maxWidth: "1295px",
     margin: "auto",
-    padding: "0 1rem",
+    // padding: "0 1rem",
   };
   return <div style={container}>{props.children}</div>;
 }
